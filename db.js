d83a383c51e3754390e4077f239a01db778faac6
MichaelMahon <michaelmahon92@verizon.net>
2016-09-15 13:59:32 -0400

    added backend functions

---
+++ db.js
@@ -0,0 +1,86 @@
+var sqlite3 = require('sqlite3');
+var db = new sqlite3.Database("insta.sqlite", sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE);
+
+exports.getUserIdByName = getUserIdByName;
+function getUserIdByName(userName) {
+	    return new Promise(
+        (resolve, reject) => {          
+            db.all("SELECT UserID FROM User WHERE UserName = ?", userName,
+                function (err, rows) {
+                    if (err) {
+                        console.log(err);
+                        reject(err);
+                        return false;
+                    }                    
+                    resolve(rows);
+                });
+        }).then(
+        (rows) => {
+            var jsonrows = JSON.parse(JSON.stringify(rows));
+            return jsonrows[0].UserID;
+        }
+        ).catch(
+        (err) => {
+            console.log(err);
+        });
+}
+
+function uploadImage() {
+
+}
+
+exports.getUserImages = getUserImages;
+function getUserImages(userID) {
+	    return new Promise(
+        (resolve, reject) => {          
+            db.all("SELECT DISTINCT i.ImgID, i.ImgName, I.PostDate, i.ImgLocation FROM Image as i WHERE i.UserID = ? ORDER BY i.PostDate desc", userID,
+                function (err, rows) {
+                    if (err) {
+                        console.log(err);
+                        reject(err);
+                        return false;
+                    }         
+                    resolve(rows);
+                });
+        }).then(
+        (rows) => {
+            console.log("userImages rows: " + rows);
+            var jsonrows = JSON.stringify(rows);
+            console.log("userImages json: " + jsonrows);
+            return jsonrows;
+        }
+        ).catch(
+        (err) => {
+            console.log(err);
+        });
+ }
+
+exports.getUserFeed = getUserFeed;
+function getUserFeed(userID) {
+	return new Promise(
+        (resolve, reject) => {          
+            db.all("SELECT DISTINCT i.ImgID, i.ImgName, I.PostDate, i.ImgLocation FROM Image as i, User as u WHERE i.UserID in ( select UserFollowed from Follow as f where f.UserID = ?)  ORDER BY i.PostDate desc", userID,
+                function (err, rows) {
+                    if (err) {
+                        console.log(err);
+                        reject(err);
+                        return false;
+                    }         
+                    resolve(rows);
+                });
+        }).then(
+        (rows) => {
+            console.log("selectUserFeeds rows: " + rows);
+            var jsonrows = JSON.stringify(rows);
+            console.log("selectUserFeeds json: " + jsonrows);
+            return jsonrows;
+        }
+        ).catch(
+        (err) => {
+            console.log(err);
+        });
+}
+
+//var testUser = 4;
+//getUserImages(testUser);
+//getUserFeed(testUser);
