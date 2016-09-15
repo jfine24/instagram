1a333e8395593cfc0383d0890722aab9ff643c5a
MichaelMahon <michaelmahon92@verizon.net>
2016-09-15 13:59:32 -0400

    added backend functions

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-var express = require('express');
+var express = require('express'),
+	app = express();
+	sqlite3 = require('sqlite3').verbose(),
+    db = new sqlite3.Database('insta.sqlite'),
+    fs = require('fs'),
+    dbGetUserIDByName = require('./db.js').GetUserIDByName,
+    dbgetUserImages = require('./db.js').getUserImages,
+	dbgetUserFeed = require('./db.js').getUserFeed,    
+	bodyParser = require('body-parser'),
+	path = require('path');
 
-var app = express();
+	app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+    extended: true
+})); 
 
+app.use('/static', express.static(__dirname + '/build'));
 
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+});
 
 app.listen(3000, function () {
     console.log('Listening on port 3000');
--- build/bundle.js
+++ build/bundle.js
@@ -44,7 +44,7 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	/* REACT HOT LOADER */ if (false) { (function () { var ReactHotAPI = require("C:\\Users\\admin\\instagram\\node_modules\\react-hot-api\\modules\\index.js"), RootInstanceProvider = require("C:\\Users\\admin\\instagram\\node_modules\\react-hot-loader\\RootInstanceProvider.js"), ReactMount = require("react/lib/ReactMount"), React = require("react"); module.makeHot = module.hot.data ? module.hot.data.makeHot : ReactHotAPI(function () { return RootInstanceProvider.getRootInstances(ReactMount); }, React); })(); } try { (function () {
+	/* REACT HOT LOADER */ if (false) { (function () { var ReactHotAPI = require("C:\\Users\\admin\\Documents\\SSA 4Week\\web\\instagram\\node_modules\\react-hot-api\\modules\\index.js"), RootInstanceProvider = require("C:\\Users\\admin\\Documents\\SSA 4Week\\web\\instagram\\node_modules\\react-hot-loader\\RootInstanceProvider.js"), ReactMount = require("react/lib/ReactMount"), React = require("react"); module.makeHot = module.hot.data ? module.hot.data.makeHot : ReactHotAPI(function () { return RootInstanceProvider.getRootInstances(ReactMount); }, React); })(); } try { (function () {
 	
 	"use strict";
 	
@@ -73,7 +73,7 @@
 	        var _this = _possibleConstructorReturn(this, (Main.__proto__ || Object.getPrototypeOf(Main)).call(this));
 	
 	        _this.state = {
-	            clickCount: 0
+	            clickCount: 5
 	        };
 	        return _this;
 	    }
@@ -107,7 +107,7 @@
 	
 	(0, _reactDom.render)(_react2.default.createElement(Main, null), document.getElementById('main'));
 	
-	/* REACT HOT LOADER */ }).call(this); } finally { if (false) { (function () { var foundReactClasses = module.hot.data && module.hot.data.foundReactClasses || false; if (module.exports && module.makeHot) { var makeExportsHot = require("C:\\Users\\admin\\instagram\\node_modules\\react-hot-loader\\makeExportsHot.js"); if (makeExportsHot(module, require("react"))) { foundReactClasses = true; } var shouldAcceptModule = true && foundReactClasses; if (shouldAcceptModule) { module.hot.accept(function (err) { if (err) { console.error("Cannot not apply hot update to " + "main.js" + ": " + err.message); } }); } } module.hot.dispose(function (data) { data.makeHot = module.makeHot; data.foundReactClasses = foundReactClasses; }); })(); } }
+	/* REACT HOT LOADER */ }).call(this); } finally { if (false) { (function () { var foundReactClasses = module.hot.data && module.hot.data.foundReactClasses || false; if (module.exports && module.makeHot) { var makeExportsHot = require("C:\\Users\\admin\\Documents\\SSA 4Week\\web\\instagram\\node_modules\\react-hot-loader\\makeExportsHot.js"); if (makeExportsHot(module, require("react"))) { foundReactClasses = true; } var shouldAcceptModule = true && foundReactClasses; if (shouldAcceptModule) { module.hot.accept(function (err) { if (err) { console.error("Cannot not apply hot update to " + "main.js" + ": " + err.message); } }); } } module.hot.dispose(function (data) { data.makeHot = module.makeHot; data.foundReactClasses = foundReactClasses; }); })(); } }
 
 /***/ },
 /* 1 */
