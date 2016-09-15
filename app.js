95f5bdfb5f0d9746d62dc8d69114e63262260fa3
jfine24 <jfine24@gmail.com>
2016-09-15 13:01:39 -0400

    Add some static folders and redirect index.html

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var express = require('express');
-
 var app = express();
 
-
+app.use(express.static('html'));
+app.use(express.static('js'));
+app.use(express.static('css'));
+app.use(express.static('build'));
 
 app.listen(3000, function () {
     console.log('Listening on port 3000');
+});
+
+app.get('/', function (req, res) {
+    res.redirect('index.html');
 });
