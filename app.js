a4d21692efb5d57566f7a831cf9f5f5ce3837d7f
MichaelMahon <michaelmahon92@verizon.net>
2016-09-15 14:44:09 -0400

    added backend calls to app.js

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 var express = require('express');
 var app = express();
+var getUserFeed = require('./db.js').getUserFeed;
+var getUserImages = require('./db.js').getUserImages;
 
 app.use(express.static('html'));
 app.use(express.static('js'));
 app.use(express.static('css'));
 app.use(express.static('build'));
+
+app.get('/getUserFeeds/:userId', function(req, res) {
+     var userid = req.params.userId;
+     console.log("userid:" + userid);   
+     var p = getUserFeed(userid);
+     p.then(
+        (val) => {
+            res.send(val);
+            }
+        ).catch(
+            (err) => {
+            res.send(err);
+        });       
+});
+
+app.get('/getUserImages/:userId', function(req, res) {
+     var userid = req.params.userId;
+     console.log("userid:" + userid);   
+     var p = getUserImages(userid);
+     p.then(
+        (val) => {
+            res.send(val);
+            }
+        ).catch(
+            (err) => {
+            res.send(err);
+        });       
+});
 
 app.listen(3000, function () {
     console.log('Listening on port 3000');
--- build/bundle.js
+++ build/bundle.js
@@ -105,6 +105,35 @@
 	    return Main;
 	}(_react2.default.Component);
 	
+	function getUserFeeds() {
+	    var xhttp = new XMLHttpRequest();
+	    var feedUrl = "http://localhost:3000/getUserFeeds/" + 4 + "/";
+	
+	    console.log(feedUrl);
+	    console.log(xhttp.responseText);
+	
+	    xhttp.onreadystatechange = function () {
+	        if (xhttp.readyState === 4 && xhttp.status === 200) {
+	            var obj = JSON.parse(xhttp.responseText);
+	            for (var i = 0; i < obj.length; i++) {
+	                // console.log(i + " -> " + obj[i]['tweetText']);   
+	                var cp = document.createElement('span');
+	                var userTweethref = 'http://localhost:8080/getUserTweets/' + obj[i]['authorID'] + '/';
+	                console.log("userTweethref  -> " + userTweethref);
+	                cp.innerHTML = '<a id="userTweets" onclick="window.open(\'' + userTweethref + '\', \'pop\', \'top=50,left=500,width=400,height=600,resizable=1,scrollbars=1,titlebar=1\');"><u>' + obj[i]['UserName'] + '</u></a>' + '&nbsp;&nbsp;&nbsp;' + obj[i]['lastupdated'] + '<br/>' + obj[i]['tweetText'] + '<br/>' + '<button id="replyBtn' + obj[i]['tweetID'] + '" onclick="getReplies(' + obj[i]['tweetID'] + ')">Replies</button><br/><div id="replies' + obj[i]['tweetID'] + '""></div><br/>';
+	                if (i % 2 != 0) {
+	                    cp.style['background-color'] = '#d3d3d3';
+	                }
+	                document.getElementById("feeds").appendChild(cp);
+	                cp = null;
+	                hasReplies(obj[i]['tweetID']);
+	            }
+	        }
+	    };
+	    xhttp.open("GET", feedUrl, true);
+	    xhttp.send();
+	}
+	
 	(0, _reactDom.render)(_react2.default.createElement(Main, null), document.getElementById('main'));
 	
 	/* REACT HOT LOADER */ }).call(this); } finally { if (false) { (function () { var foundReactClasses = module.hot.data && module.hot.data.foundReactClasses || false; if (module.exports && module.makeHot) { var makeExportsHot = require("C:\\Users\\admin\\Documents\\SSA 4Week\\web\\instagram\\node_modules\\react-hot-loader\\makeExportsHot.js"); if (makeExportsHot(module, require("react"))) { foundReactClasses = true; } var shouldAcceptModule = true && foundReactClasses; if (shouldAcceptModule) { module.hot.accept(function (err) { if (err) { console.error("Cannot not apply hot update to " + "main.js" + ": " + err.message); } }); } } module.hot.dispose(function (data) { data.makeHot = module.makeHot; data.foundReactClasses = foundReactClasses; }); })(); } }
