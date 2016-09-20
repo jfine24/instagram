5352345d3fcef0e78160879bd7b86c5505269952
MichaelMahon <michaelmahon92@verizon.net>
2016-09-20 12:30:29 -0400

    working on angular

--- js/default.js
+++ js/default.js
@@ -1,38 +1,63 @@
-  $(function() {
-    $.post("/default", null, function(result){
-        if (result.error) {
-          window.location.replace("/login.html");
-        } else {
-          $('#userName').html(result.userName);
-          getUserFeeds(result.userID);
-        }
-    }, "json");
-  });
 
-  function getUserFeeds(id) {
-    var br = document.createElement('br');
-    var param = '/getUserFeeds/'+id;
-    $.getJSON(param, function(result){
-      if (result.error) {
-        $('#main').html(result.error);
-      } else {
-        var feeds = document.getElementById('feeds');
-        feeds.innerHTML = '';
-        $.each(result, function(i, field) {
-          feeds.appendChild(br);
-          var d = document.createElement('div');
-          d.id = "Block"+field["ImgID"];
-          d.className = "block";
-          var img = document.createElement('img');
-          img.id = "Img"+ field["ImgID"];
-          img.src = 'getImage/' + field["UserID"] + "/" + field["ImgLocation"];
-          img.width = '400';
-          d.appendChild(img);
-          var p = document.createElement('p');
-          p.innerHTML = field['ImgName'] + ' (' + field['PostDate'] + ')';
-          d.appendChild(p);
-          feeds.appendChild(d);
-        });
-      }      
-    }, "json");    
-   }
+
+var instagram = angular.module('instagram', []);
+      instagram.controller('instaCtrl', function ($scope, $http, $window){
+        
+        $scope.login = function() {
+          var user = $scope.user;
+          console.log(user);
+          $http.post("/login/ + user", function(result){
+          console.log(result);
+        })
+        }
+        
+
+        $scope.getUserFeed = function() {
+          console.log("in function");
+          $http.get("/getUserFeeds/1").success(function(data) {
+          console.log("in function");
+          $scope.results = data;
+          });
+        }
+
+      });
+
+
+  // $(function() {
+  //   $.post("/default", null, function(result){
+  //       if (result.error) {
+  //         window.location.replace("/login.html");
+  //       } else {
+  //         $('#userName').html(result.userName);
+  //         getUserFeeds(result.userID);
+  //       }
+  //   }, "json");
+  // });
+
+  // function getUserFeeds(id) {
+  //   var br = document.createElement('br');
+  //   var param = '/getUserFeeds/'+id;
+  //   $.getJSON(param, function(result){
+  //     if (result.error) {
+  //       $('#main').html(result.error);
+  //     } else {
+  //       var feeds = document.getElementById('feeds');
+  //       feeds.innerHTML = '';
+  //       $.each(result, function(i, field) {
+  //         feeds.appendChild(br);
+  //         var d = document.createElement('div');
+  //         d.id = "Block"+field["ImgID"];
+  //         d.className = "block";
+  //         var img = document.createElement('img');
+  //         img.id = "Img"+ field["ImgID"];
+  //         img.src = 'getImage/' + field["UserID"] + "/" + field["ImgLocation"];
+  //         img.width = '400';
+  //         d.appendChild(img);
+  //         var p = document.createElement('p');
+  //         p.innerHTML = field['ImgName'] + ' (' + field['PostDate'] + ')';
+  //         d.appendChild(p);
+  //         feeds.appendChild(d);
+  //       });
+  //     }      
+  //   }, "json");    
+  //  }
--- js/main.js
+++ js/main.js
@@ -1,57 +1,50 @@
-import React from "react";
-import {render} from 'react-dom';
 
-class Main extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            clickCount: 5,
-        };
-    }
-    render() {
-        return <div>
-            <a href="" onClick={this.handleClick.bind(this)}>Clicks: {this.state.clickCount}</a>
-        </div>;
-    }
-    handleClick(event) {
-        event.preventDefault();
-        this.setState({
-            clickCount: this.state.clickCount + 1,
-        });
-    }
-}
 
-function getUserFeeds(userID) {
-  var xhttp = new XMLHttpRequest();
-  var feedUrl= "http://localhost:3000/getUserFeeds/" + userID + "/";
+
+var instagram = angular.module('instagram', []);
+      instagram.controller('instaCtrl', function ($scope, $http){
+        
+
+        $scope.getUserFeed = function() {
+          console.log("in function");
+          $http.get("/getUserFeeds/1").success(function(data) {
+          console.log("in function");
+          $scope.results = data;
+          });
+        }
+
+      });
+// function getUserFeeds(userID) {
+//   var xhttp = new XMLHttpRequest();
+//   var feedUrl= "http://localhost:3000/getUserFeeds/" + userID + "/";
   
-  console.log(feedUrl);
-  console.log(xhttp.responseText);
+//   console.log(feedUrl);
+//   console.log(xhttp.responseText);
   
-  xhttp.onreadystatechange = function() {
- if (xhttp.readyState === 4 && xhttp.status === 200) { 
-     var obj = JSON.parse(xhttp.responseText);        
-     for (var i = 0; i < obj.length; i++) {           
-            // console.log(i + " -> " + obj[i]['tweetText']);   
-            var cp =  document.createElement('span');            
-            var userTweethref='http://localhost:8080/getUserTweets/' + obj[i]['authorID']  + '/';    
-            console.log("userTweethref  -> " + userTweethref);            
-            cp.innerHTML = '<a id="userTweets" onclick="window.open(\'' + userTweethref + '\', \'pop\', \'top=50,left=500,width=400,height=600,resizable=1,scrollbars=1,titlebar=1\');"><u>' +  obj[i]['UserName'] + '</u></a>' + 
-            '&nbsp;&nbsp;&nbsp;' + obj[i]['lastupdated'] + '<br/>' + obj[i]['tweetText'] + '<br/>' +
-            '<button id="replyBtn' +  obj[i]['tweetID'] + '" onclick="getReplies(' + obj[i]['tweetID'] + ')">Replies</button><br/><div id="replies' +  obj[i]['tweetID'] + '""></div><br/>';
-             if (i % 2 != 0) {
-                 cp.style['background-color'] = '#d3d3d3';
-         } 
-        document.getElementById("feeds").appendChild(cp);   
-        cp = null;
-        hasReplies(obj[i]['tweetID']);  
-      } 
-    } 
-  };
-  xhttp.open("GET", feedUrl, true);
-  xhttp.send();
-}
+//   xhttp.onreadystatechange = function() {
+//  if (xhttp.readyState === 4 && xhttp.status === 200) { 
+//      var obj = JSON.parse(xhttp.responseText);        
+//      for (var i = 0; i < obj.length; i++) {           
+//             // console.log(i + " -> " + obj[i]['tweetText']);   
+//             var cp =  document.createElement('span');            
+//             var userTweethref='http://localhost:8080/getUserTweets/' + obj[i]['authorID']  + '/';    
+//             console.log("userTweethref  -> " + userTweethref);            
+//             cp.innerHTML = '<a id="userTweets" onclick="window.open(\'' + userTweethref + '\', \'pop\', \'top=50,left=500,width=400,height=600,resizable=1,scrollbars=1,titlebar=1\');"><u>' +  obj[i]['UserName'] + '</u></a>' + 
+//             '&nbsp;&nbsp;&nbsp;' + obj[i]['lastupdated'] + '<br/>' + obj[i]['tweetText'] + '<br/>' +
+//             '<button id="replyBtn' +  obj[i]['tweetID'] + '" onclick="getReplies(' + obj[i]['tweetID'] + ')">Replies</button><br/><div id="replies' +  obj[i]['tweetID'] + '""></div><br/>';
+//              if (i % 2 != 0) {
+//                  cp.style['background-color'] = '#d3d3d3';
+//          } 
+//         document.getElementById("feeds").appendChild(cp);   
+//         cp = null;
+//         hasReplies(obj[i]['tweetID']);  
+//       } 
+//     } 
+//   };
+//   xhttp.open("GET", feedUrl, true);
+//   xhttp.send();
+// }
 
-render((
-    <Main/>
-), document.getElementById('main'));
+// render((
+//     <Main/>
+// ), document.getElementById('main'));
